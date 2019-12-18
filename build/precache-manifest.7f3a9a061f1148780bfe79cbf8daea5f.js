b472c7397d50f28c0297ad859f25d9a7f64ec2b0
Your Name <1282505174@qq.com>
2019-12-18 19:54:56 +0800

    first-try

---
+++ build/precache-manifest.7f3a9a061f1148780bfe79cbf8daea5f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535dafd3550d6e10e582827c127c9ac3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8b3435a5f318eb06276",
+    "url": "./static/css/main.d920d24a.chunk.css"
+  },
+  {
+    "revision": "fb08e25812c810a2eab4",
+    "url": "./static/js/2.265ba74d.chunk.js"
+  },
+  {
+    "revision": "d8b3435a5f318eb06276",
+    "url": "./static/js/main.587a0b93.chunk.js"
+  },
+  {
+    "revision": "7593cb9770e7aa1493af",
+    "url": "./static/js/runtime-main.78bad60c.js"
+  },
+  {
+    "revision": "65f002153652d9c6d06ccf49cf49fef6",
+    "url": "./static/media/iconfont.65f00215.ttf"
+  },
+  {
+    "revision": "848270a57fe7056175a7af1e8d222a1a",
+    "url": "./static/media/iconfont.848270a5.woff"
+  },
+  {
+    "revision": "a90b4919e8e19b09b06b442cdbc28cdb",
+    "url": "./static/media/iconfont.a90b4919.svg"
+  },
+  {
+    "revision": "d4f2f2a78ba650e7cb3f71c858187a28",
+    "url": "./static/media/iconfont.d4f2f2a7.eot"
+  },
+  {
+    "revision": "66431406a85987cbcd491a8953fa2747",
+    "url": "./static/media/l1.66431406.jpg"
+  },
+  {
+    "revision": "f0284593683dd10801fdd8b8aba39942",
+    "url": "./static/media/l2.f0284593.jpg"
+  },
+  {
+    "revision": "0de197a51027a81a4c8f78e9a9eadad0",
+    "url": "./static/media/l3.0de197a5.jpg"
+  },
+  {
+    "revision": "de7d51ec3a441499e3c301c060eabf99",
+    "url": "./static/media/l4.de7d51ec.jpg"
+  },
+  {
+    "revision": "9ffeff92f6965244d8b04d37c23fbbe2",
+    "url": "./static/media/l5.9ffeff92.jpg"
+  },
+  {
+    "revision": "c59e0de1725101dfa052eedb25de685e",
+    "url": "./static/media/l6.c59e0de1.jpg"
+  },
+  {
+    "revision": "9bbb3f54b71119236d1017a34cecd49d",
+    "url": "./static/media/l7.9bbb3f54.jpg"
+  },
+  {
+    "revision": "5221afc048f3e90d746fe9212c2345fe",
+    "url": "./static/media/l8.5221afc0.jpg"
+  }
+]);
